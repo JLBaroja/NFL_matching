f6391ccd79f6f30930474a2e7845b27617185eef
Luis Baroja <j.luis.baroja@gmail.com>
2024-07-03 13:53:19 -0700

    Correctly sampling from a BetaRoot

--- stan_betabin_example.R
+++ stan_betabin_example.R
@@ -1,18 +1,19 @@
 rm(list=ls())
 library('rstan')
-k <- rbinom(n=10,size=1,prob=0.3)
+full <- rbinom(n=10,size=1,prob=0.3)
 alpha_prior <- 5
 beta_prior <- 2
-observed <- list(x=sum(k),n=length(k),a_prior=alpha_prior,b_prior=beta_prior)
+root <- 2
+observed <- list(x=sum(full),n=length(full),k=root,a_prior=alpha_prior,b_prior=beta_prior)
 stan_model_code <-'
 functions{
 				real beta_root_lpdf(real th, real a, real b,real k){
-					//return ((th)^(a-1)*(1-th)^(b-1)/beta(a,b))^(1/k);
-					return (1/k)*((th)^(a-1)*(1-th)^(b-1)/beta(a,b));
+					return beta_lpdf(th|a,b)/k;
 				}
 }
 
 data{
+				int<lower=1> k;
 				int<lower=0> x;
 				int<lower=0> n;
 				int<lower=0> a_prior;
@@ -25,12 +26,8 @@ parameters{
 }
 
 model{
-				//theta_prior ~ beta(a_prior,b_prior);
-				//theta ~ beta(a_prior,b_prior);
-				theta_prior ~ beta_root(a_prior,b_prior,3);
-				theta ~ beta_root(a_prior,b_prior,3);
-				//target += beta_root(theta_prior | a_prior,b_prior,2);
-				//target += beta_root(theta | a_prior,b_prior,2);
+				theta_prior ~ beta_root(a_prior,b_prior,k);
+				theta ~ beta_root(a_prior,b_prior,k);
 				x ~ binomial(n,theta);
 }
 '
@@ -42,8 +39,8 @@ nds <- extract(fit)
 # Closed-form solution (full data)
 sup_th <- seq(0,1,0.001)
 closed_prior <- dbeta(sup_th,shape1=alpha_prior,shape2=beta_prior)
-closed_post <- dbeta(sup_th,shape1=alpha_prior+sum(k==1),
-										 shape2=beta_prior+sum(k==0))
+closed_post <- dbeta(sup_th,shape1=alpha_prior+sum(full==1),
+										 shape2=beta_prior+sum(full==0))
 beta_root <- function(th,a,b,k){
 	# Calculates the Kth root of a Beta(a,b) distribution
 	dens <- ((th^(a-1)*(1-th)^(b-1))/beta(a,b))^(1/k)
@@ -57,4 +54,5 @@ hist(nds$theta,xlim=c(0,1),breaks=brks,freq=F,col='#aaaaaa')
 hist(nds$theta_prior,add=T,breaks=brks,freq=F,col='#14c7d388')
 lines(sup_th,closed_post,lwd=2)
 lines(sup_th,closed_prior,lwd=2,lty='dashed')
-lines(sup_th,beta_root(sup_th,alpha_prior,beta_prior,3),lwd=2,col='red')
+lines(sup_th,beta_root(sup_th,alpha_prior,beta_prior,root),lwd=2,col='red')
+
