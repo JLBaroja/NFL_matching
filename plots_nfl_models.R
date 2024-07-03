bf39411425442d865e6aca7aac9652f2fa70f7ae
Luis Baroja <j.luis.baroja@gmail.com>
2024-07-03 02:56:22 -0700

    Sample plot in .pdf

--- plots_nfl_models.R
+++ plots_nfl_models.R
@@ -13,38 +13,9 @@ nds_m1 <- bayes_m1$BUGSoutput$sims.list
 nds_m2 <- bayes_m2$BUGSoutput$sims.list
 nds_m3 <- bayes_m3$BUGSoutput$sims.list
 
-colors_quarters <- c('red','blue','green','orange','cyan')
 colors_quarters <- c('#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00')
-try(dev.off())
-x11(width=17,height=8)
-layout(matrix(c(1,1,1,1,2,3,4,5,0,6,0,7),nrow=4,byrow=F),
-	widths=c(2,1,1),heights=c(1,2,1,2))
 
-# Data
-plot(NULL,xlim=c(-1,3),ylim=c(-1,3))
-kernels <- vector(mode='list',length=5)
-for(qr in 1:5){
-	qr_dta <- subset(nfl,quarter==qr)
-	x <- log(qr_dta$yards_pass/qr_dta$yards_rush)
-	y <- log(qr_dta$n_pass/qr_dta$n_rush)
-	kernels[[qr]] <- kde2d(x,y)
-	points(x,y,col=paste(colors_quarters[qr],'36',sep=''),cex=1.5,pch=qr,lwd=2)
-	dataEllipse(x,y,add=T,col=colors_quarters[qr],levels=0.95,pch=NA)
-}
-#for(qr in 1:5){
-#	qr_dta <- subset(nfl,quarter==qr)
-#	x <- log(qr_dta$yards_pass/qr_dta$yards_rush)
-#	y <- log(qr_dta$n_pass/qr_dta$n_rush)
-#	dataEllipse(x,y,add=T,col=colors_quarters[qr],levels=0.95,pch=NA)
-#	#contour(kernels[[qr]],levels=1,add=T,lwd=3,drawlabels=F,col=colors_quarters[qr])
-#}
-legend(-1,3,legend=paste(c('1st','2nd','3rd','4th','5th'),'Quarter'),col=colors_quarters,
- pch=1:5,cex=2)
-abline(0,1,lty='dashed')
-abline(v=0,lty='dashed')
-abline(h=0,lty='dashed')
-mtext('Grouping plays by team & quarter & year',3,line=1,cex=1.5)
-
+# Plotting functions
 multi_marginals <- function(node,def=500,xlimz=c(0,1),ylimz=c(0,25),
 														palette=NULL,title=NULL){
 				if(is.null(palette)){palette<-rep('#000000',dim(node)[2])}
@@ -75,6 +46,35 @@ interval_marginals <- function(node,order=F,xlimz=c(-.5,.5),cols=NULL,rct_width=
 	}
 }
 
+#try(dev.off())
+#x11(width=17,height=8)
+pdf(file='plot_nfl_models.pdf',width=17,height=8)
+layout(matrix(c(1,1,1,1,2,3,4,5,0,6,0,7),nrow=4,byrow=F),
+	widths=c(2,1,1),heights=c(1,2,1,2))
+# Data
+plot(NULL,xlim=c(-1,3),ylim=c(-1,3))
+kernels <- vector(mode='list',length=5)
+for(qr in 1:5){
+	qr_dta <- subset(nfl,quarter==qr)
+	x <- log(qr_dta$yards_pass/qr_dta$yards_rush)
+	y <- log(qr_dta$n_pass/qr_dta$n_rush)
+	kernels[[qr]] <- kde2d(x,y)
+	points(x,y,col=paste(colors_quarters[qr],'36',sep=''),cex=1.5,pch=qr,lwd=2)
+	dataEllipse(x,y,add=T,col=colors_quarters[qr],levels=0.95,pch=NA)
+}
+#for(qr in 1:5){
+#	qr_dta <- subset(nfl,quarter==qr)
+#	x <- log(qr_dta$yards_pass/qr_dta$yards_rush)
+#	y <- log(qr_dta$n_pass/qr_dta$n_rush)
+#	dataEllipse(x,y,add=T,col=colors_quarters[qr],levels=0.95,pch=NA)
+#	#contour(kernels[[qr]],levels=1,add=T,lwd=3,drawlabels=F,col=colors_quarters[qr])
+#}
+legend(-1,3,legend=paste(c('1st','2nd','3rd','4th','5th'),'Quarter'),col=colors_quarters,
+ pch=1:5,cex=2)
+abline(0,1,lty='dashed')
+abline(v=0,lty='dashed')
+abline(h=0,lty='dashed')
+mtext('Grouping plays by team & quarter & year',3,line=1,cex=1.5)
 par(mar=c(1,4,4,2))
 multi_marginals(nds_m2$alpha,xlimz=c(-.5,.5),palette=colors_quarters,title='Intercept (Bias)')
 par(mar=c(5,4,0,2))
@@ -88,3 +88,4 @@ interval_marginals(nds_m3$alpha,order=T)
 interval_marginals(nds_m3$beta,order=T,xlimz=c(0,1))
 #multi_marginals(nds_m3$alpha,def=200,xlimz=c(-.5,.5),ylimz=c(0,12),title='Intercept (Bias)')
 #multi_marginals(nds_m3$beta,,def=200,ylimz=c(0,12),title='Slope (Sensitivity)')
+dev.off()
